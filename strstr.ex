dcb77c07c50e960aac0b23db8f7982c6335247bc
selfup <boudinot.regis@yahoo.com>
2019-04-13 23:00:54 -0500

    parts 2 - perf down to 7s from 11s

--- strstr.ex
+++ strstr.ex
@@ -1,4 +1,4 @@
 File.stream!("tmp/logs.log")
-|> Stream.map(&(String.trim(&1) |> String.split("OK ") |> Enum.at(1)))
+|> Stream.map(&(String.trim(&1) |> String.split("OK ", parts: 2) |> Enum.at(1)))
 |> Enum.filter(&(&1 != nil))
 |> IO.inspect()
