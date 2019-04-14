8e37c1c1eca58bb99a7b18e1c426f9f8ce0c1428
Regis <boudinot.regis@yahoo.com>
2019-04-13 20:14:51 -0500

    update go

--- strstr.go
+++ strstr.go
@@ -12,28 +12,26 @@ func main() {
 	lines := []string{}
 
 	file, err := os.Open("tmp/logs.log")
-	if err != nil {
-		log.Fatal(err)
-	}
+	check(err)
 
 	defer file.Close()
 
-	// doesn't do great with lines with more than 65536 characters
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		if len(line) > 2 {
-			if strings.Contains(line, "OK db=") {
-				split := strings.Split(line, "OK ")
-				lines = append(lines, split[1])
-			}
+		if strings.Contains(line, "OK db=") {
+			split := strings.Split(line, "OK ")
+			lines = append(lines, split[1])
 		}
 	}
 
-	if err := scanner.Err(); err != nil {
+	check(scanner.Err())
+	fmt.Println(lines[0:40])
+}
+
+func check(err error) {
+	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println(lines[0:40])
 }
